094e51232e9a20c9087ce81ee3c69aafcbb8ed9b
yhthia <e1408755@U.nus.edu>
2025-08-20 12:38:29 +0800

    Add add, list

--- src/main/java/MyChatBot.java
+++ src/main/java/MyChatBot.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class MyChatBot {
     private final Scanner input = new Scanner(System.in);
+    private ArrayList<String> list = new ArrayList<>();
+
 
     private void greet() {
         System.out.println("Hello! I'm MyChatBot.");
@@ -12,14 +16,28 @@ public class MyChatBot {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    private void add(String userInput) {
+        list.add(userInput);
+        System.out.println("added: " + userInput);
+    }
+
+    private void printList() {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(" " + (i + 1) + ". " + list.get(i));
+        }
+    }
+
     private void echo() {
         while (true) {
             String userInput = input.nextLine();
             if (userInput.equals("bye")) {
                 exit();
                 break;
+            } else if (userInput.equals("list")) {
+                printList();
+            } else {
+                add(userInput);
             }
-            System.out.println(userInput);
         }
     }
 
