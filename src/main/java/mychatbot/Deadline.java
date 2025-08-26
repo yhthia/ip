30de9559322fbe9f97ef5d18c212fee3babc5d72
yhthia <e1408755@U.nus.edu>
2025-08-26 18:31:47 +0800

    Add Javadoc comments for Deadline class methods

--- src/main/java/mychatbot/Deadline.java
+++ src/main/java/mychatbot/Deadline.java
@@ -3,21 +3,40 @@ package mychatbot;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a Deadline that has a description, completion status and a due date.
+ * Extends the Task class to include a due date.
+ */
 public class Deadline extends Task {
     protected LocalDateTime by;
 
+    /**
+     * Constructs a Deadline.
+     * The due date (parameter 'by') should be in the format 'd/M/yyyy HHmm'.
+     * @param description
+     * @param isDone
+     * @param by
+     */
     public Deadline(String description, boolean isDone, String by) {
         super(description, isDone);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
         this.by = LocalDateTime.parse(by, formatter);
     }
 
+    /**
+     * Returns the string representation of the Deadline, formatted for displaying to the user.
+     * @return
+     */
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
         return "[D]" + super.toString() + " (by: " + by.format(formatter) + ")";
     }
 
+    /**
+     * Returns the string representation of the Deadline, formatted for file storage.
+     * @return
+     */
     @Override
     public String toDataString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
