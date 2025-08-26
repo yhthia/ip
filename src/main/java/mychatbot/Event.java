851bd9b15f1908821a2bc9b2fb5f81392e7a32be
yhthia <e1408755@U.nus.edu>
2025-08-26 18:33:43 +0800

    Add Javadoc comments for Event class methods

--- src/main/java/mychatbot/Event.java
+++ src/main/java/mychatbot/Event.java
@@ -3,10 +3,23 @@ package mychatbot;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * Represents a Event that has a description, completion status, start and end dates.
+ * Extends the Task class to include start and end dates.
+ */
 public class Event extends Task {
     protected LocalDateTime from;
     protected LocalDateTime to;
 
+    /**
+     * Constructs an Event.
+     * The start and end dates (parameter 'from' and 'to' respectively),
+     * should be in the format 'd/M/yyyy HHmm'.
+     * @param description
+     * @param isDone
+     * @param from
+     * @param to
+     */
     public Event(String description, boolean isDone, String from, String to) {
         super(description, isDone);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
@@ -14,12 +27,20 @@ public class Event extends Task {
         this.to = LocalDateTime.parse(to, formatter);
     }
 
+    /**
+     * Returns the string representation of the Event, formatted for displaying to the user.
+     * @return
+     */
     @Override
     public String toString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
         return "[E][" + getStatusIcon() + "] " + description + " (from: " + from.format(formatter) + " to: " + to.format(formatter) + ")";
     }
 
+    /**
+     * Returns the string representation of the Event, formatted for file storage.
+     * @return
+     */
     @Override
     public String toDataString() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
