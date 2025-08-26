61cec34cebe1b597b4c90c8c3e673ee99a16bc18
yhthia <e1408755@U.nus.edu>
2025-08-26 18:22:19 +0800

    Add Javadoc comments for Parser class methods

--- src/main/java/mychatbot/Parser.java
+++ src/main/java/mychatbot/Parser.java
@@ -1,11 +1,26 @@
 package mychatbot;
 
+/**
+ * Provides static methods for parsing users' input based on task type.
+ */
 public class Parser {
+    /**
+     * Extracts the command from the user input, which is the first word of the input.
+     * @param input The raw input string.
+     * @return The command.
+     */
     public static String getCommandType(String input) {
         String[] tokens = input.trim().split(" ", 2);
         return tokens[0];
     }
 
+    /**
+     * Parses the task index from the user input.
+     * Converts it into a zero based index.
+     * @param input The raw input string.
+     * @return The new task index.
+     * @throws MyChatBotException If the format or number of the input string is invalid.
+     */
     public static int getTaskIndex(String input) throws MyChatBotException {
         try {
             String[] tokens = input.trim().split(" ");
@@ -24,10 +39,20 @@ public class Parser {
         }
     }
 
+    /**
+     * Extracts the description of the Todo from the user input.
+     * @param input The raw input string.
+     * @return The description of the Todo in the user input.
+     */
     public static String getDescription(String input) {
         return input.substring(input.indexOf(" ") + 1).trim();
     }
 
+    /**
+     * Extracts the description and deadline of the Deadline from the user input.
+     * @param input The raw input string.
+     * @return The description and deadline of the Deadline in the user input as String[].
+     */
     public static String[] getDeadlineParts(String input) {
         String body = input.substring(8).trim();
         int byIdx = body.indexOf(" /by ");
@@ -36,6 +61,11 @@ public class Parser {
         return new String[]{desc.trim(), by.trim()};
     }
 
+    /**
+     * Extracts the description, start and end times of the Event from the user input.
+     * @param input The raw input string.
+     * @return The description, start and end times of the Event in the user input as String[].
+     */
     public static String[] getEventParts(String input) {
         String body = input.substring(5).trim();
         int fromIdx = body.indexOf(" /from ");
