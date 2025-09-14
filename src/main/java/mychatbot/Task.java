28f50b31007f15e66564f0bf658d860b1f1b9332
yhthia <e1408755@U.nus.edu>
2025-09-14 16:53:39 +0800

    Add JavaDoc comments to public methods in Task and TaskList

--- src/main/java/mychatbot/Task.java
+++ src/main/java/mychatbot/Task.java
@@ -6,27 +6,57 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+
+    /**
+     * Constructs a Task with the specified description and completion status.
+     *
+     * @param description The description of the task.
+     * @param isDone True if the task is completed and false otherwise.
+     */
     public Task(String description, boolean isDone) {
         this.description = description;
         this.isDone = isDone;
     }
 
+    /**
+     * Returns the description of the task.
+     *
+     * @return The task description.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Returns the date and time associated with the task.
+     * For todos, this returns null.
+     *
+     * @return The date and time of the task, or null if not applicable.
+     */
     public LocalDateTime getDateTime() {
         return null;
     }
 
+    /**
+     * Returns the status icon representing whether the task is done.
+     * "X" for done, " " for not done.
+     *
+     * @return The status icon.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Marks the task as undone.
+     */
     public void markAsNotDone() {
         this.isDone = false;
     }
--- src/main/java/mychatbot/TaskList.java
+++ src/main/java/mychatbot/TaskList.java
@@ -11,18 +11,41 @@ public class TaskList {
         this.tasks = new ArrayList<>();
     }
 
+
+    /**
+     * Creates a TaskList with the specified tasks.
+     *
+     * @param tasks The initial list of tasks.
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds a task to the task list.
+     *
+     * @param task The task to be added.
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Returns the list of all tasks.
+     *
+     * @return The ArrayList of tasks.
+     */
     public ArrayList<Task> getTasks() {
         return tasks;
     }
 
+    /**
+     * Retrieves the task at the specified index.
+     *
+     * @param idx The index of the task to retrieve.
+     * @return The task at the specified index.
+     * @throws MyChatBotException If the index is out of bounds.
+     */
     public Task getTask(int idx) throws MyChatBotException {
         if (idx < 0 || idx >= tasks.size()) {
             throw new MyChatBotException("Task number is out of bounds.");
@@ -30,10 +53,21 @@ public class TaskList {
         return tasks.get(idx);
     }
 
+    /**
+     * Returns the number of tasks in the list.
+     *
+     * @return The size of the task list.
+     */
     public int size() {
         return tasks.size();
     }
 
+    /**
+     * Finds tasks whose description contains the given keyword (case-insensitive).
+     *
+     * @param keyword The keyword to search for.
+     * @return A list of tasks that match the keyword.
+     */
     public ArrayList<Task> findTasks(String keyword) {
         ArrayList<Task> matchingTasks = new ArrayList<>();
         if (keyword == null || keyword.isEmpty()) {
@@ -47,6 +81,10 @@ public class TaskList {
         return matchingTasks;
     }
 
+    /**
+     * Sorts the tasks chronologically by their datetime.
+     * Tasks without a date/time will be placed at the bottom.
+     */
     public void sortChronologically() {
         tasks.sort(Comparator.comparing(
                 task -> task.getDateTime() == null ? LocalDateTime.MAX : task.getDateTime()
